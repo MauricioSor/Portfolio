b3b3ca81ac95c7550e71b66f1476d7675c389624
Mauricio Soria <mauricioutn2017@gmail.com>
2023-08-29 23:51:43 -0300

    feature:menu desplegable a la izquierda

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -6,10 +6,59 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer'; // Asegúrate de agregar esta línea
+import List from '@mui/material/List';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
 
 const Menu = () => {
+    const [state, setState] = React.useState({left: false,});
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (
+            event &&
+            event.type === 'keydown' &&
+            (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <Box 
+            sx={{width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}>
+            <List >
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton >
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            {/* Contenido del cajón */}
+            {/* Reemplaza esto con el contenido real que deseas mostrar en el cajón */}
+        </Box>
+    );
+
     return (
-            <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton
@@ -17,9 +66,17 @@ const Menu = () => {
                         edge="start"
                         color="inherit"
                         aria-label="menu"
-                        sx={{ mr: 2 }}>
+                        sx={{ mr: 2 }}
+                        onClick={toggleDrawer('left', true)}> {/* Open the drawer when clicked */}
                         <MenuIcon />
                     </IconButton>
+                    <SwipeableDrawer
+                        anchor="left"
+                        open={state.left}
+                        onClose={toggleDrawer('left', false)}
+                        onOpen={toggleDrawer('left', true)}>
+                        {list('left')}
+                    </SwipeableDrawer>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         News
                     </Typography>
