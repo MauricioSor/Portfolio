86314a5cbcb3063ca3708dea73c013428bf728c9
Mauricio Soria <mauricioutn2017@gmail.com>
2023-08-30 00:03:40 -0300

    Correccion responsive

--- src/components/views/Principal.jsx
+++ src/components/views/Principal.jsx
@@ -2,8 +2,8 @@ import { Container } from "react-bootstrap";
 import muestra from "../../assets/foto.png"
 const Principal = () => {
     return (
-        <Container className="text-center">
-        <img src={muestra} alt="" />
+        <Container className="text-center main d-flex align-items-center">
+        <img src={muestra} alt="" className="w-100 " />
         </Container>
     );
 };
