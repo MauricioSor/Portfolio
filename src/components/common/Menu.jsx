8d6b7819dd0e3168e06668733914bb437a68d02f
Mauricio Soria <mauricioutn2017@gmail.com>
2023-12-19 18:39:47 -0300

    fix bugs

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
-import {Nav,Navbar,Container} from "react-bootstrap"
-
+import {Nav,Navbar,Container,Image} from "react-bootstrap"
+import Logo from "../../assets/logo1.png"
 const Menu = () => {
 
     return (
         <>
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
+                <Navbar.Brand  className='d-flex align-items-center justify-content-start'>
+                    <Image src={Logo} style={{height:"50px"}} alt=""className='mx-2' roundedCircle/>
+                    <p className='ms-2 mt-3'>Mauricio Agustin Soria </p>
+                </Navbar.Brand>
+                    <Nav className="me-0">
                         <Nav.Link href="#home">Inicio</Nav.Link>
                         <Nav.Link href="#pricing">Biografia</Nav.Link>
                         <Nav.Link href="#features">Contacto</Nav.Link>
--- src/components/views/Principal.jsx
+++ src/components/views/Principal.jsx
@@ -5,14 +5,16 @@ import MatrixAnimation from "./MatrixAnimation";
 const Principal = () => {
     return (
         <>
-        <MatrixAnimation className="absolute inset-0" />
-        <div className="flex flex-col">
-        <Biografia/>
-        {/* <SoftSkillss/> */}
-        <Tecnologias/>
-        {/* <Proyectos/> */}
-        {/* <Contacto/> */}
-        </div>
+            <div className="min-h-screen">
+            <MatrixAnimation className="absolute inset-0" id="fondo" />
+                <div className="flex flex-col">
+                    <Biografia />
+                    {/* <SoftSkillss/> */}
+                    <Tecnologias />
+                    {/* <Proyectos/> */}
+                    {/* <Contacto/> */}
+                </div>
+            </div>
         </>
     );
 };
