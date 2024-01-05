73282dc8884f310a7f5785c5d11aea0c46e9049e
Mauricio Soria <mauricioutn2017@gmail.com>
2024-01-05 15:29:48 -0300

    fix link

--- src/components/views/AcercaDeMi/Certificados.jsx
+++ src/components/views/AcercaDeMi/Certificados.jsx
@@ -9,7 +9,7 @@ const Certificados = () => {
         <h3 className='fs-2 texto text-center my-2'>Certificados</h3>
         </Col>
         <Col className="align-self-center text-center" lg={5}md={12} xs={12}>
-        <a id="boton" target="_blank" href='https://frtutneduar-my.sharepoint.com/personal/mauriciosoria_alu_frt_utn_edu_ar/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fmauriciosoria%5Falu%5Ffrt%5Futn%5Fedu%5Far%2FDocuments%2FCertificados%2FCertificados%2D2023%2Epdf&parent=%2Fpersonal%2Fmauriciosoria%5Falu%5Ffrt%5Futn%5Fedu%5Far%2FDocuments%2FCertificados&ga=1' className='btn text-decoration-none'>Certificados de cursos y competencias en linea <i class="bi bi-cloud-upload-fill"id='boton'/></a>
+        <a id="boton" target="_blank" href='https://drive.google.com/file/d/1EpSmyjO21NF-BcEX6WuQRdgOj8GMO4Ac/view?usp=sharing' className='btn text-decoration-none'>Certificados de cursos y competencias en linea <i class="bi bi-cloud-upload-fill"id='boton'/></a>
         </Col>
         </Row>
         </Container>
