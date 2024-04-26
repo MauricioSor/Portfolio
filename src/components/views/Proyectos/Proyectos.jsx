4fb4edf5a077d507595481eed2ce9b36c0e414a9
Mauricio Soria <mauricioutn2017@gmail.com>
2024-04-26 18:54:50 -0300

    feature:imagenes del proyecto agregadas

--- src/components/views/Proyectos/Proyectos.jsx
+++ src/components/views/Proyectos/Proyectos.jsx
@@ -270,7 +270,7 @@ const Proyectos = () => {
         {
             id:20,
             title:"Tucu Libre",
-            image:[Imagen88,Imagen89,Imagen90],
+            image:[Imagen92,Imagen93,Imagen94,Imagen95],
             text:"Software desarrollado específicamente para dispositivos móviles, en la plataforma de expo",
             link:"https://expo.dev/preview/update?message=Merge%20branch%20%27dev-jas%27%20of%20https%3A%2F%2Fgithub.com%2FG3-R-Avanzado%2FFrontend-Native%20into%20dev%0A%0A%23%20Conflicts%3A%0A%23%09src%2Fstore%2FSlices%2Fpublication&updateRuntimeVersion=1.0.0&createdAt=2024-04-17T22%3A10%3A01.729Z&slug=exp&projectId=feba06fb-143d-493b-9704-0b3ea8dc57ef&group=e1d0a534-3bf8-4844-b51d-323fca6d3a53",
             technologies:["React native","Javascript","MongoDB"]
@@ -278,7 +278,7 @@ const Proyectos = () => {
         {
             id:21,
             title:"Mau's Restobar",
-            image:[Imagen92,Imagen93,Imagen94,Imagen95],
+            image:[Imagen88,Imagen89,Imagen90],
             text:"Pequeño sistema genérico desarrollado con la finalidad de presentar en el final de Programación web de la carrera de ISI",
             link:"https://mau-sr-restaurant.vercel.app/",
             technologies:["React","Javascript","MongoDB","Node","Express"]
