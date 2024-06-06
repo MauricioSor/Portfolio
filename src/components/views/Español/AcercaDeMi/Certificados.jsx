92534309d50cd9481504793e6198f27037b54f03
Mauricio Soria <mauricioutn2017@gmail.com>
2024-06-06 18:31:40 -0300

    feature: new link to redirect certificates

--- src/components/views/Español/AcercaDeMi/Certificados.jsx
+++ src/components/views/Español/AcercaDeMi/Certificados.jsx
@@ -9,7 +9,7 @@ const Certificados = () => {
         <h3 className='fs-2 texto text-center my-2'><u className='subrayados'>Certificados</u></h3>
         </Col>
         <Col className="align-self-center text-center" lg={5}md={12} xs={12}>
-        <a id="botonCertificados" target="_blank" href='https://drive.google.com/file/d/1EpSmyjO21NF-BcEX6WuQRdgOj8GMO4Ac/view?usp=sharing' className='btn text-decoration-none'>Certificados de cursos y competencias en linea <i class="bi bi-cloud-upload-fill"id='boton'/></a>
+        <a id="botonCertificados" target="_blank" href='https://drive.google.com/file/d/1GW4XF0t4_lJXO6ZX-opYlUviFaEMfmhe/view?usp=sharing' className='btn text-decoration-none'>Certificados de cursos y competencias en linea <i class="bi bi-cloud-upload-fill"id='boton'/></a>
         </Col>
         </Row>
         </Container>
